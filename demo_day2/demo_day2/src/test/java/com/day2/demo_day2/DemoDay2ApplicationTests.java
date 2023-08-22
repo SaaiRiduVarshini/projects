e1e1e78ae8faa3c4e15fa2e693d449e22f16ac0f
SaaiRiduVarshini <108523980+SaaiRiduVarshini@users.noreply.github.com>
2023-08-22 10:20:12 +0530

    Add files via upload

---
+++ demo_day2/demo_day2/src/test/java/com/day2/demo_day2/DemoDay2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day2.demo_day2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDay2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
